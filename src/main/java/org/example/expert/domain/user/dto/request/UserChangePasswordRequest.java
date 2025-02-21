3c9297f41ce216be128cee0358de223e3ba287c2
HoKirby <lakevely27@gmail.com>
2025-02-21 14:32:10 +0900

    refactor: 1-3 validation
    spring boot validation 어노테이션 사용해 런타임 시점에 유효성 검사 진행

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,6 @@
 package org.example.expert.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +12,8 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "새 비밀번호는 숫자와 영어 대문자를 포함해야합니다")
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야합니다")
+    @NotBlank(message = "새 비밀번호는 필수 입력값입니다")
     private String newPassword;
 }
