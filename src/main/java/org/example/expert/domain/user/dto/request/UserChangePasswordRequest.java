0971905e6b341faa6fc0afb14ca47c973b504483
MJK-One <hanelejss012@naver.com>
2025-04-18 17:08:22 +0900

    LV 1. 코드개선

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
-
     @NotBlank
     private String oldPassword;
+
+    // 코드 개선 퀴즈 - Validation
     @NotBlank
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이여야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "새 비밀번호는 숫자를 포함해야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호는 대문자를 포함해야 합니다.")
     private String newPassword;
 }
